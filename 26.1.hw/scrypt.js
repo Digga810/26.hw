a8ecd32848d91acd874dc3f6e289eda3cc5ad5bb
qwerty <nagib@gmail.com>
2021-11-14 15:25:32 +0500

    first commit

---
+++ 26.1.hw/scrypt.js
@@ -0,0 +1,78 @@
+var x = +prompt('Введите ваше время');
+switch (x) {
+    case 1:
+        alert('1 час ночи')
+        break;
+    case 2:
+        alert('2 час ночи')
+        break;
+    case 3:
+        alert('3 час ночи')
+        break;
+    case 4:
+        alert('4 час ночи')
+        break;
+    case 5:
+        alert('5 час утра')
+        break;
+    case 6:
+        alert('6 час утра')
+        break;
+    case 7:
+        alert('7 час утра')
+        break;
+    case 8:
+        alert('8 час утра')
+        break;
+    case 9:
+        alert('9 час утра')
+        break;
+    case 10:
+        alert('10 час утра')
+        break;
+    case 11:
+        alert('11 час дня')
+        break;
+    case 12:
+        alert('12 час дня')
+        break;
+    case 13:
+        alert('1 час дня')
+        break;
+    case 14:
+        alert('2 час дня')
+        break;
+    case 15:
+        alert('3 час дня')
+        break;
+    case 16:
+        alert('4 час дня')
+        break;
+    case 17:
+        alert('5 час вечера')
+        break;
+    case 18:
+        alert('6 час вечера')
+        break;
+    case 19:
+        alert('7 час вечера')
+        break;
+    case 20:
+        alert('8 час вечера')
+        break;
+    case 21:
+        alert('9 час ночи')
+        break;
+    case 22:
+        alert('10 час ночи')
+        break;
+    case 23:
+        alert('11 час ночи')
+        break;
+    case 24:
+        alert('12 час ночи')
+        break;
+    default:
+        alert('ОШИБКА')
+        break;
+}
